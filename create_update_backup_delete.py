694a9831e72d5a1211c92069e00e46446c6d5148
BruceBanner001 <subramanianthayuman@gmail.com>
2025-09-12 00:09:11 +0530

    changed sheet to Feb 7 2023 Onwards

--- create_update_backup_delete.py
+++ create_update_backup_delete.py
@@ -99,7 +99,7 @@ _sheets_env = os.environ.get("SHEETS", "").strip()
 if _sheets_env:
     SHEETS = [s.strip() for s in _sheets_env.split(";") if s.strip()]
 else:
-    SHEETS = ["Sheet1;Feb 7 2023 Onwards"]
+    SHEETS = ["Sheet1","Feb 7 2023 Onwards"]
   # include any that exist
 # SHEETS = ["Feb 7 2023 Onwards"]  # include any that exist
 
